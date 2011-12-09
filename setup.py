df5709751294b9eaa2c4ce6d275870baf19ba780
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-12-09 19:21:46 +0100

    Initial structure

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2011 Linaro Limited
+#
+# Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
+#
+# This file is part of LAVA Serial 
+#
+# LAVA Serial is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3
+# as published by the Free Software Foundation
+#
+# LAVA Serial is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with LAVA Serial.  If not, see <http://www.gnu.org/licenses/>.
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print "This package requires setuptools to be configured"
+    print "It can be installed with debian/ubuntu package python-setuptools"
+    raise
+
+
+setup(
+    name='lava-vcs',
+    version=":versiontools:lava.serial:",
+    author="Zygmunt Krynicki",
+    author_email="zygmunt.krynicki@linaro.org",
+    namespace_packages=['lava'],
+    packages=['lava.vcs', 'lava.serial'],
+    license="AGPL",
+    entry_points="""
+    [lava_tool.commands]
+    serial = lava.serial.commands:SerialCommand
+    """,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Testing"],
+    install_requires=[
+        'versiontools >= 1.8',
+        'lava-tool >= 2.0',
+        'pyserial',
+    ],
+    setup_requires=[
+        'versiontools >= 1.8'
+    ],
+    zip_safe=True,
+    include_package_data=True
+)

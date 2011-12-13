0cee33cac10eeefe0b13ceef3543bc7a2a1790d9
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-12-13 14:36:51 +0100

    Add lava.serial.direct
    This file implements an enhanced version of Serial
    class as provided by python-serial. The improvements
    use exclusive locking to prevent other instances
    from opening (and corrupting) the serial device.

---
+++ lava/serial/direct.py
@@ -0,0 +1,31 @@
+import fcntl
+import errno
+
+import serial as pyserial
+
+
+class DirectSerialLine(pyserial.Serial):
+    """
+    A subclass of serial.Serial that implements exclusive locking on
+    the serial device
+    """
+
+    def open(self):
+        """
+        Open the serial port and lock the file descriptor used by the serial
+        line. Locking will be pefromed only if there is a file descriptor
+        method to call.
+        """
+        super(DirectSerialLine, self).open()
+        if not hasattr(self, "fileno"):
+            return
+        fd = self.fileno()
+        try:
+            fcntl.flock(fd, fcntl.LOCK_EX|fcntl.LOCK_NB)
+        except IOError as exc:
+            if exc.errno == errno.EAGAIN:
+                self.close()
+                raise pyserial.SerialException(
+                    "Unable to lock the serial line (perhaps somene is using it)")
+            else:
+                raise
